0adafe7c92c177e9a39a5d0b5261c35a24b22877
Astrophysics108 <dsingh@perse.co.uk>
2025-06-02 12:05:01 +0100

    added a wallpaper feature , quite concerningly anki is not even starting up

--- initialise_ui_components/init_ui.py
+++ initialise_ui_components/init_ui.py
@@ -1,5 +1,6 @@
 from aqt import *
 from aqt.qt import *
+import os
 
 # basic text window
 def basicText(title, words):
@@ -10,28 +11,75 @@ def basicText(title, words):
     dlg.setLayout(layout)
     dlg.exec()
 
-# adds a new action that just makes a basic text window
-def newactionbasic(name, text, menu, func=basicText):
-    newaction =  QAction(name, mw)
-    connectbasic = lambda func: func(name, text)
-    newaction.triggered.connect(connectbasic(func))
+# adds a new action that can call any function with optional arguments
+def newactionbasic(name, text, menu, func=None, *args, **kwargs):
+    newaction = QAction(name, mw)
+    if func is None:
+        func = basicText
+        action = lambda: func(name, text)
+    else:
+        action = lambda: func(*args, **kwargs)
+    newaction.triggered.connect(action)
     menu.addAction(newaction)
 
 def wallpaper():
-    # This function can be used to set a wallpaper or background image
-    # For now, it does nothing but can be expanded later
-    pass
+    # Path to store the selected wallpaper path
+    config_dir = os.path.join(mw.pm.profileFolder(), "make_anki_beautiful")
+    os.makedirs(config_dir, exist_ok=True)
+    wallpaper_path_file = os.path.join(config_dir, "wallpaper_path.txt")
+
+    # Check if a wallpaper has already been set
+    if os.path.exists(wallpaper_path_file):
+        with open(wallpaper_path_file, "r", encoding="utf-8") as f:
+            wallpaper_path = f.read().strip()
+        if os.path.exists(wallpaper_path):
+            set_anki_wallpaper(wallpaper_path)
+            basicText("Wallpaper", "Wallpaper already set.")
+            return
+
+    # Ask user to select an image file from Desktop
+    desktop = os.path.join(os.path.expanduser("~"), "Desktop")
+    file_dialog = QFileDialog(mw)
+    file_dialog.setDirectory(desktop)
+    file_dialog.setNameFilter("Images (*.png *.jpg *.jpeg *.bmp *.gif)")
+    if file_dialog.exec():
+        selected_files = file_dialog.selectedFiles()
+        if selected_files:
+            wallpaper_path = selected_files[0]
+            # Save the selected path
+            with open(wallpaper_path_file, "w", encoding="utf-8") as f:
+                f.write(wallpaper_path)
+            set_anki_wallpaper(wallpaper_path)
+            basicText("Wallpaper", "Wallpaper set successfully.")
+        else:
+            basicText("Wallpaper", "No file selected.")
+    else:
+        basicText("Wallpaper", "No file selected.")
+
+def set_anki_wallpaper(image_path):
+    # Inject CSS to set the background image for Anki main window
+    image_path_fixed = image_path.replace("\\", "/")
+    css = f"""
+    QMainWindow {{
+        background-image: url("{image_path_fixed}");
+        background-repeat: no-repeat;
+        background-position: center;
+        background-attachment: fixed;
+        background-size: cover;
+    }}
+    """
+    mw.setStyleSheet(css)
 
 # add tools
 def addUi(menu):
     actions = {
         "Test" : "test",
         "Info" : "Addon to make anki beautiful",
-        "Wallpaper" : "Set a wallpaper or background image"
     }
     for name in actions.keys():
         text = actions[name]
         newactionbasic(name, text, menu)
+    newactionbasic("Wallpaper", "Set a wallpaper or background image", menu, func=wallpaper)
 
 #init
 def initui(menu):
