251f35e9d5720dcf038c35d437176f6d042f9d4a
Astrophysics108 <dsingh@perse.co.uk>
2025-06-02 13:34:15 +0100

    okay so anki isnt crashing anymore but for some reason QMainWindow isn't what I need to change but I can't figure out what it is that I need to change aaagh

--- initialise_ui_components/init_ui.py
+++ initialise_ui_components/init_ui.py
@@ -23,12 +23,10 @@ def newactionbasic(name, text, menu, func=None, *args, **kwargs):
     menu.addAction(newaction)
 
 def wallpaper():
-    # Path to store the selected wallpaper path
     config_dir = os.path.join(mw.pm.profileFolder(), "make_anki_beautiful")
     os.makedirs(config_dir, exist_ok=True)
     wallpaper_path_file = os.path.join(config_dir, "wallpaper_path.txt")
 
-    # Check if a wallpaper has already been set
     if os.path.exists(wallpaper_path_file):
         with open(wallpaper_path_file, "r", encoding="utf-8") as f:
             wallpaper_path = f.read().strip()
@@ -37,7 +35,6 @@ def wallpaper():
             basicText("Wallpaper", "Wallpaper already set.")
             return
 
-    # Ask user to select an image file from Desktop
     desktop = os.path.join(os.path.expanduser("~"), "Desktop")
     file_dialog = QFileDialog(mw)
     file_dialog.setDirectory(desktop)
@@ -46,7 +43,6 @@ def wallpaper():
         selected_files = file_dialog.selectedFiles()
         if selected_files:
             wallpaper_path = selected_files[0]
-            # Save the selected path
             with open(wallpaper_path_file, "w", encoding="utf-8") as f:
                 f.write(wallpaper_path)
             set_anki_wallpaper(wallpaper_path)
@@ -56,12 +52,16 @@ def wallpaper():
     else:
         basicText("Wallpaper", "No file selected.")
 
+# def initwallpaper():
+#     config_dir = os.path.join(mw.pm.profileFolder(), "make_anki_beautiful")
+#     os.makedirs(config_dir, exist_ok=True)
+#     wallpaper_path_file = os.path.join(config_dir, "wallpaper_path.txt")
+
 def set_anki_wallpaper(image_path):
-    # Inject CSS to set the background image for Anki main window
     image_path_fixed = image_path.replace("\\", "/")
     css = f"""
     QMainWindow {{
-        background-image: url("{image_path_fixed}");
+        background-image: url("{image_path_fixed}") !important;
         background-repeat: no-repeat;
         background-position: center;
         background-attachment: fixed;
@@ -84,3 +84,4 @@ def addUi(menu):
 #init
 def initui(menu):
     addUi(menu)
+    
