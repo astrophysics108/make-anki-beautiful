1da2bf1e7b5fb025ae7f3c2889f9f180a267509b
Astrophysics108 <dsingh@perse.co.uk>
2025-06-02 11:40:10 +0100

    more versatility in newavctionbasic (ANKI IS NOT CRASHING NOW!!!)

--- initialise_ui_components/init_ui.py
+++ initialise_ui_components/init_ui.py
@@ -11,16 +11,23 @@ def basicText(title, words):
     dlg.exec()
 
 # adds a new action that just makes a basic text window
-def newactionbasic(name, text, menu):
+def newactionbasic(name, text, menu, func=basicText):
     newaction =  QAction(name, mw)
-    newaction.triggered.connect(lambda: basicText(name, text))
+    connectbasic = lambda func: func(name, text)
+    newaction.triggered.connect(connectbasic(func))
     menu.addAction(newaction)
+
+def wallpaper():
+    # This function can be used to set a wallpaper or background image
+    # For now, it does nothing but can be expanded later
+    pass
 
 # add tools
 def addUi(menu):
     actions = {
         "Test" : "test",
-        "Info" : "Addon to make anki beautiful"
+        "Info" : "Addon to make anki beautiful",
+        "Wallpaper" : "Set a wallpaper or background image"
     }
     for name in actions.keys():
         text = actions[name]
